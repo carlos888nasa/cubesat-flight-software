1224cf1e9355f76e5c41aa49c5a81d0ef66e3137
carlos888nasa <carlosortegamantero@gmail.com>
2026-02-04 23:26:56 +0100

    The MIT license has been added

---
+++ obc/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "core/scheduler.h"
+
+int main() {
+    printf("[OBC] Iniciando sistema de vuelo...\n");
+    printf("[OBC] Estado: NOMINAL\n");
+    printf("[OBC] Esperando al Scheduler...\n");
+    
+    // Aquí irá el bucle infinito del satélite más adelante
+    
+    return 0;
+}
