11c18f49568e183e15f710f5680845b70fd1e992
carlos888nasa <carlosortegamantero@gmail.com>
2026-02-07 00:15:20 +0100

    Basic algebra functions have been added that will be useful in the future

--- obc/utils/math_utils.c
+++ obc/utils/math_utils.c
@@ -2,12 +2,12 @@
 #include <math.h> 
 #include "math_utils.h"
 
-vec3_add(Vec3 a, Vec3 b){
+Vec3 vec3_add(Vec3 a, Vec3 b){
     Vec3 result; 
     result.x = a.x + b.x;
     result.y = a.y + b.y;
     result.z = a.z + b.z;
-    return result; 
+    return result;
 }
 
 Vec3 vec3_sub(Vec3 a, Vec3 b){
@@ -15,5 +15,37 @@ Vec3 vec3_sub(Vec3 a, Vec3 b){
     result.x = a.x - b.x;
     result.y = a.y - b.y;
     result.z = a.z - b.z;
+    return result; 
+}
+
+Vec3 vec3_scale(Vec3 v, float scalar){
+    Vec3 result; 
+    result.x = v.x * scalar;
+    result.y = v.y * scalar;
+    result.z = v.z * scalar;
+    return result; 
+}
+
+float vec3_length(Vec3 v){
+    return sqrtf(powf(v.x, 2) + powf(v.y, 2) + powf(v.z, 2));
+}
+
+Vec3 vec3_normalize(Vec3 v){
+    Vec3 result; 
+    float magnitud = sqrtf(powf(v.x, 2) + powf(v.y, 2) + powf(v.z, 2));
+    result.x = v.x/magnitud;
+    result.y = v.y/magnitud;
+    result.z = v.z/magnitud; 
+}
+
+float vec3_dot(Vec3 a, Vec3 b){
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+Vec3 vec3_cross(Vec3 a, Vec3 b){
+    Vec3 result; 
+    result.x = a.y * b.z - a.z * b.y;
+    result.y = a.z * b.x - a.x * b.z;
+    result.z = a.x * b.y - a.y * b.x;
     return result; 
 }
